5fc24f5d2f5254af68ca3844c9dbead3c9e1e674
Jules Moreno <julesmoreno66@gmail.com>
2023-04-09 15:59:46 +0800

    keyboard.jsx: add style

--- src/features/Keyboard/index.jsx
+++ src/features/Keyboard/index.jsx
@@ -1,5 +1,22 @@
+import Keyboard from 'react-simple-keyboard';
+import 'react-simple-keyboard/build/css/index.css';
+import './styles/keyboard.css';
+
 export default function KeyBoard() {
   return (
-    <div> KeyBoard</div>
+    <section className="w-[40em] mb-6">
+      <Keyboard
+        onChange=""
+        onKeyPress=""
+        theme="hg-theme-default hg-layout-default"
+        layout={{
+          default: [
+            'Q W E R T Y U I O P',
+            'A S D F G H J K L',
+            '&#x2B05 Z X C V B N M Enter',
+          ],
+        }}
+      />
+    </section>
   );
 }
